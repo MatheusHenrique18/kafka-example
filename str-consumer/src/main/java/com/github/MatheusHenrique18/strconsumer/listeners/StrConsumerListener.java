5d4c8ac7b2085c6c46257ea48f58cd138ffbc560
MatheusHenrique18 <matheus.hsb18@gmail.com>
2022-06-17 16:45:18 -0300

    incluindo interceptor, para validar a mensagem antes de ser consumida pelo listener.

--- str-consumer/src/main/java/com/github/MatheusHenrique18/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/github/MatheusHenrique18/strconsumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package com.github.MatheusHenrique18.strconsumer.listeners;
 
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.github.MatheusHenrique18.strconsumer.custom.StrConsumerCustomListener;
@@ -20,7 +21,7 @@ public class StrConsumerListener {
 		log.info("LOG ::: Receive message {}", message);
 	}
 	
-	@StrConsumerCustomListener(groupId = "group-2")
+	@KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
 	public void history(String message) {
 		log.info("HISTORY ::: Receive message {}", message);
 	}
