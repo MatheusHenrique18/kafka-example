a364e29f9347f7f33b05113147ec804ba86e3353
MatheusHenrique18 <matheus.hsb18@gmail.com>
2022-06-17 16:23:23 -0300

    adicionando listener customizado.

--- str-consumer/src/main/java/com/github/MatheusHenrique18/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/github/MatheusHenrique18/strconsumer/listeners/StrConsumerListener.java
@@ -1,7 +1,8 @@
 package com.github.MatheusHenrique18.strconsumer.listeners;
 
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import com.github.MatheusHenrique18.strconsumer.custom.StrConsumerCustomListener;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -9,13 +10,34 @@ import lombok.extern.log4j.Log4j2;
 @Component
 public class StrConsumerListener {
 
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+	@StrConsumerCustomListener(groupId = "group-1")
 	public void create(String message) {
 		log.info("CREATE ::: Receive message {}", message);
 	}
 	
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+	@StrConsumerCustomListener(groupId = "group-1")
 	public void log(String message) {
 		log.info("LOG ::: Receive message {}", message);
 	}
+	
+	@StrConsumerCustomListener(groupId = "group-2")
+	public void history(String message) {
+		log.info("HISTORY ::: Receive message {}", message);
+	}
+	
+//	@KafkaListener(groupId = "group-1", 
+//			topicPartitions = {
+//					@TopicPartition(topic = "str-topic", partitions = {"0"})
+//			}, containerFactory = "strContainerFactory")
+//	public void create(String message) {
+//		log.info("LOG ::: Receive message {}", message);
+//	}
+	
+//	@KafkaListener(groupId = "group-1", 
+//			topicPartitions = {
+//					@TopicPartition(topic = "str-topic", partitions = {"1"})
+//			}, containerFactory = "strContainerFactory")
+//	public void log(String message) {
+//		log.info("LOG ::: Receive message {}", message);
+//	}
 }
